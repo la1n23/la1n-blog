05f8fb3b4e53f83568cd8525a8150cf25539df9c
la1n23 <la1n23@proton.me>
2025-09-23 00:59:07 +0300

    Reapply "Fix urls"
    This reverts commit 963a5729215b21ded16c512771d2ec666e57abc6.

--- src/site.config.ts
+++ src/site.config.ts
@@ -30,11 +30,11 @@ const config: SiteConfig = {
   navLinks: [
     {
       name: 'Home',
-      url: '/',
+      url: '/la1n-blog',
     },
     {
       name: 'Posts',
-      url: '/posts',
+      url: '/la1n-blog/posts',
     },
     {
       name: 'GitHub',
@@ -45,7 +45,7 @@ const config: SiteConfig = {
   // The theming configuration for the site.
   themes: {
     // The theming mode. One of "single" | "select" | "light-dark-auto".
-    mode: 'select',
+    mode: 'single',
     // The default theme identifier, used when themeMode is "select" or "light-dark-auto".
     // Make sure this is one of the themes listed in `themes` or "auto" for "light-dark-auto" mode.
     default: 'solarized-dark',
